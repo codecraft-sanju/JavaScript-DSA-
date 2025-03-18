f4bcb3a7c9cbf39902397e7f15d8e261263a1209
codecraft-sanju <sanjusirvich18@gmail.com>
2025-03-18 15:00:14 +0530

    arrmax

--- ArrMax.js
+++ ArrMax.js
@@ -3,9 +3,19 @@ function findMax(arr) {
   let max = arr[0];
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] > max) {    //  25 > 10    45>25   2>45  89>45   14>89   max=89
-      max = arr[i];         //  max=25     //max=45  //max=45  //max=89  
+      max = arr[i];         //  max=25     //max=45  //max=45  //max=89
     }
   }
   return max;
 }
 console.log(findMax(numbers));
+
+
+// let arr = [45, 34, 89, 23]
+// let max = Math.max(...arr)   //...arr (spread operator)
+// console.log(max);
+
+
+
+
+
