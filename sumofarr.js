3027684dad8f125fbf68e54ed76e0f62e434095a
codecraft-sanju <sanjusirvich18@gmail.com>
2025-03-18 23:08:08 +0530

    sumofarray

--- sumofarr.js
+++ sumofarr.js
@@ -1,6 +1,16 @@
-let arr = [3, 6, 2, 9, 4]
+// let arr = [3, 6, 2, 9, 4]
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     sum = sum + arr[i];
+// }
+// return sum;
+
+
+let arr = [2, 3, 4]
 let sum = 0;
-for (let i = 0; i < arr.length; i++) {   
-    sum = sum + arr[i];  
+let i = 0;
+while (i < arr.length) {
+    sum=sum+arr[i]
 }
-return sum;
+console.log(sum);
+
