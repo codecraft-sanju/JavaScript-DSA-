ed42c29a76d73ee6e641ba492e3ff3824ea4afa7
codecraft-sanju <sanjusirvich18@gmail.com>
2025-03-20 10:52:55 +0530

    selection sort

--- SelectionSort.js
+++ SelectionSort.js
@@ -1,10 +1,11 @@
 let arr = [45, 23, 90, 20]
-for (let i = 0; i < arr.length-1; i++){
-  let MinimumIndex = i;
-  for (let j = i + 1; j < arr.length; j++){
-    if (arr[j] < arr[MinimumIndex]) {
-      MinimumIndex = j;
-   }
+for (let i = 0; i < arr.length-1; i++){   //45  
+  let MinimumIndex = i;                //i=0,1,2,3  
+  for (let j = i + 1; j < arr.length; j++){  //23,90,20
+    if (arr[j] < arr[MinimumIndex]) {   //23,90,20<45    23<45 true   90<45false   20<45true
+      MinimumIndex = j;                                  //j=1  minimumindex=1 //j=3 minimumindex=3
+    }                                           // minimumindex=i=0,1,2,3   j=1,2,3 //45,23,90,20
+                                             //20,23,45,90   
   }
   let temp = arr[i]
   arr[i] = arr[MinimumIndex]
