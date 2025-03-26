77e95c474ec5a5afa691613d9f419c4e3fb92392
codecraft-sanju <sanjusirvich18@gmail.com>
2025-03-26 14:58:30 +0530

    secondmaxarr

--- SecondMaxArr.js
+++ SecondMaxArr.js
@@ -1,18 +1,17 @@
-var arr = [-93, -45, -12,-67, -89];
+var arr = [-93, -45, -12, -67, -89];
+let max = -Infinity;
+let secondmax = -Infinity;
 
-function secondLargestElement(arr) {
-  var first = -Infinity; // Using -Infinity to handle cases where all elements are negative
-  var second = -Infinity; // Ensures the correct second largest number is found
-
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > first) {
-      second = first;
-      first = arr[i];
-    } else if (arr[i] > second && arr[i] != first) {
-      second = arr[i];
+function seconndLargest(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {  // If arr[i] is the new max
+      secondmax = max; // Update second max
+      max = arr[i]; // Update max
+    } else if (arr[i] > secondmax && arr[i] != max) { // Check for second max
+      secondmax = arr[i];
     }
   }
-  return second;
+  return secondmax; // Return the second largest number
 }
 
-console.log(secondLargestElement(arr));
+console.log(seconndLargest(arr)); // Output: 89
