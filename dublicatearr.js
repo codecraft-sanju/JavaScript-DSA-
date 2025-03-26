9df7169f45ec393ec9b7e9fad5386aaab0f91ce6
codecraft-sanju <sanjusirvich18@gmail.com>
2025-03-26 15:23:44 +0530

    dublicatefind

---
+++ dublicatearr.js
@@ -0,0 +1,16 @@
+let arr = [34, 56, 23, 78, 23, 67, 56]; 
+
+function dublicateValue(arr) {
+  let dublicate = []; 
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      // If a duplicate is found and it's not already in the duplicate array
+      if (arr[i] == arr[j] && !dublicate.includes(arr[i])) {
+        dublicate.push(arr[i]); 
+      }
+    }
+  }
+  return dublicate; 
+}
+
+console.log(dublicateValue(arr)); 
