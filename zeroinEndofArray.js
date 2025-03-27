b46c1b2858187b2f57aad8e567efead5025531b7
codecraft-sanju <sanjusirvich18@gmail.com>
2025-03-27 15:32:44 +0530

    zeroinendarray

--- zeroinEndofArray.js
+++ zeroinEndofArray.js
@@ -1,17 +1,39 @@
-function shiftZerosToRight(arr) {
-    let lastNonZeroIndex = 0; // 1
+// function shiftZerosToRight(arr) {
+//     let lastNonZeroIndex = 0; // 1
 
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] !== 0) {                    
-            arr[lastNonZeroIndex] = arr[i];  //arr[0]=34   
-            if (i != lastNonZeroIndex) { //0 != 0
-              arr[i] = 0; ////arr[i]=0
-            }
-            lastNonZeroIndex++;    //0 to 1
+//     for (let i = 0; i < arr.length; i++){
+//         if (arr[i] !== 0) {
+//             arr[lastNonZeroIndex] = arr[i];  //arr[0]=34
+//             if (i != lastNonZeroIndex) { //0 != 0
+//               arr[i] = 0; ////arr[i]=0
+//             }
+//             lastNonZeroIndex++;    //0 to 1
             
-        }
+//         }
+//     }
+//     return arr;
+// }
+// let arr = [34, 0, 23, 45, 0, 2, 4];
+// console.log(shiftZerosToRight(arr));
+
+
+
+function shiftZerosToRight(arr) {
+  let nonZeroIndex = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      let temp = arr[i];
+      arr[i] = arr[nonZeroIndex];
+      arr[nonZeroIndex] = temp;
+      nonZeroIndex++;
     }
-    return arr;
+  }
+
+  return arr;
 }
-let arr = [34, 0, 23, 45, 0, 2, 4];
+
+let arr = [34, 0, 8, 45, 0, 0, 4];
 console.log(shiftZerosToRight(arr));
+
+
